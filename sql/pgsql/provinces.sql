3578d5304e90cc9a662955f48d4b9e87863d6be9
Farshad Fallah <49900536+FarshadFalah@users.noreply.github.com>
2022-08-24 22:45:58 +0430

    Update provinces.sql
    salam

--- sql/pgsql/provinces.sql
+++ sql/pgsql/provinces.sql
@@ -1,94 +1,42 @@
-CREATE TABLE "public"."provinces" (
-    "id" int4 NOT NULL,
-    "name" varchar(19) COLLATE "pg_catalog"."default" NOT NULL,
-    "slug" varchar(17) COLLATE "pg_catalog"."default" NOT NULL
+CREATE TABLE "public"."provinces"
+(
+  "id"   int4                                       NOT NULL,
+  "name" varchar(19) COLLATE "pg_catalog"."default" NOT NULL,
+  "slug" varchar(17) COLLATE "pg_catalog"."default" NOT NULL
 );
 
-INSERT INTO
-    "public"."provinces"
-VALUES
-    (1, 'آذربایجان شرقی', 'آذربایجان-شرقی');
-
-INSERT INTO
-    "public"."provinces"
-VALUES
-    (2, 'آذربایجان غربی', 'آذربایجان-غربی');
-
-INSERT INTO "public"."provinces" VALUES (3, 'اردبیل', 'اردبیل');
-
-INSERT INTO "public"."provinces" VALUES (4, 'اصفهان', 'اصفهان');
-
-INSERT INTO "public"."provinces" VALUES (5, 'البرز', 'البرز');
-
-INSERT INTO "public"."provinces" VALUES (6, 'ایلام', 'ایلام');
-
-INSERT INTO "public"."provinces" VALUES (7, 'بوشهر', 'بوشهر');
-
-INSERT INTO "public"."provinces" VALUES (8, 'تهران', 'تهران');
-
-INSERT INTO
-    "public"."provinces"
-VALUES
-    (9, 'چهارمحال و بختیاری', 'چهارمحال-بختیاری');
-
-INSERT INTO
-    "public"."provinces"
-VALUES
-    (10, 'خراسان جنوبی', 'خراسان-جنوبی');
-
-INSERT INTO
-    "public"."provinces"
-VALUES
-    (11, 'خراسان رضوی', 'خراسان-رضوی');
-
-INSERT INTO
-    "public"."provinces"
-VALUES
-    (12, 'خراسان شمالی', 'خراسان-شمالی');
-
-INSERT INTO "public"."provinces" VALUES (13, 'خوزستان', 'خوزستان');
-
-INSERT INTO "public"."provinces" VALUES (14, 'زنجان', 'زنجان');
-
-INSERT INTO "public"."provinces" VALUES (15, 'سمنان', 'سمنان');
-
-INSERT INTO
-    "public"."provinces"
-VALUES
-    (16, 'سیستان و بلوچستان', 'سیستان-بلوچستان');
-
-INSERT INTO "public"."provinces" VALUES (17, 'فارس', 'فارس');
-
-INSERT INTO "public"."provinces" VALUES (18, 'قزوین', 'قزوین');
-
-INSERT INTO "public"."provinces" VALUES (19, 'قم', 'قم');
-
-INSERT INTO "public"."provinces" VALUES (20, 'کردستان', 'کردستان');
-
-INSERT INTO "public"."provinces" VALUES (21, 'کرمان', 'کرمان');
-
-INSERT INTO "public"."provinces" VALUES (22, 'کرمانشاه', 'کرمانشاه');
-
-INSERT INTO
-    "public"."provinces"
-VALUES
-    (23, 'کهگیلویه و بویراحمد', 'کهگیلویه-بویراحمد');
-
-INSERT INTO "public"."provinces" VALUES (24, 'گلستان', 'گلستان');
-
-INSERT INTO "public"."provinces" VALUES (25, 'لرستان', 'لرستان');
-
-INSERT INTO "public"."provinces" VALUES (26, 'گیلان', 'گیلان');
-
-INSERT INTO "public"."provinces" VALUES (27, 'مازندران', 'مازندران');
-
-INSERT INTO "public"."provinces" VALUES (28, 'مرکزی', 'مرکزی');
-
-INSERT INTO "public"."provinces" VALUES (29, 'هرمزگان', 'هرمزگان');
-
-INSERT INTO "public"."provinces" VALUES (30, 'همدان', 'همدان');
-
-INSERT INTO "public"."provinces" VALUES (31, 'یزد', 'یزد');
+INSERT INTO "public"."provinces"
+VALUES (1, 'آذربایجان شرقی', 'آذربایجان-شرقی'),
+       (2, 'آذربایجان غربی', 'آذربایجان-غربی'),
+       (3, 'اردبیل', 'اردبیل'),
+       (4, 'اصفهان', 'اصفهان'),
+       (5, 'البرز', 'البرز'),
+       (6, 'ایلام', 'ایلام'),
+       (7, 'بوشهر', 'بوشهر'),
+       (8, 'تهران', 'تهران'),
+       (9, 'چهارمحال و بختیاری', 'چهارمحال-بختیاری'),
+       (10, 'خراسان جنوبی', 'خراسان-جنوبی'),
+       (11, 'خراسان رضوی', 'خراسان-رضوی'),
+       (12, 'خراسان شمالی', 'خراسان-شمالی'),
+       (13, 'خوزستان', 'خوزستان'),
+       (14, 'زنجان', 'زنجان'),
+       (15, 'سمنان', 'سمنان'),
+       (16, 'سیستان و بلوچستان', 'سیستان-بلوچستان'),
+       (17, 'فارس', 'فارس'),
+       (18, 'قزوین', 'قزوین'),
+       (19, 'قم', 'قم'),
+       (20, 'کردستان', 'کردستان'),
+       (21, 'کرمان', 'کرمان'),
+       (22, 'کرمانشاه', 'کرمانشاه'),
+       (23, 'کهگیلویه و بویراحمد', 'کهگیلویه-بویراحمد'),
+       (24, 'گلستان', 'گلستان'),
+       (25, 'لرستان', 'لرستان'),
+       (26, 'گیلان', 'گیلان'),
+       (27, 'مازندران', 'مازندران'),
+       (28, 'مرکزی', 'مرکزی'),
+       (29, 'هرمزگان', 'هرمزگان'),
+       (30, 'همدان', 'همدان'),
+       (31, 'یزد', 'یزد');
 
 -- ----------------------------
 
@@ -97,6 +45,6 @@ INSERT INTO "public"."provinces" VALUES (31, 'یزد', 'یزد');
 -- ----------------------------
 
 ALTER TABLE
-    "public"."provinces"
-ADD
+  "public"."provinces"
+  ADD
     CONSTRAINT "provinces_pkey" PRIMARY KEY ("id");
