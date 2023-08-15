cf71cd96a5976063b4cdbb7f53442ba10a27262c
Sajad Dehshiri <sajaddp71@gmail.com>
2023-08-15 15:51:26 +0330

    Update index.js

--- example/es6/index.js
+++ example/es6/index.js
@@ -1,12 +1,46 @@
-import { readFile } from "fs/promises";
+import cities from "./json/cities.json";
+import provinces from "./json/provinces.json";
 
-const cities = JSON.parse(
-  await readFile(new URL("../../json/cities.json", import.meta.url))
-);
+// Return all cities
+export function getAllCities() {
+  return cities;
+}
 
-const provinces = JSON.parse(
-  await readFile(new URL("../../json/provinces.json", import.meta.url))
-);
+// Return all provinces
+export function getAllProvinces() {
+  return provinces;
+}
 
-console.log("---------", cities);
-console.log("<br>", "---------", provinces);
+// Return all the cities of a specific province based on province's name
+export function getCitiesByProvinceName(name) {
+  const province = provinces.find((p) => p.name === name);
+  if (!province) return [];
+  return cities.filter((city) => city.province_id === province.id);
+}
+
+// Return all the cities of a specific province based on province's ID
+export function getCitiesByProvinceId(id) {
+  return cities.filter((city) => city.province_id === id);
+}
+
+// Return all the cities of a specific province based on province's slug
+export function getCitiesByProvinceSlug(slug) {
+  const province = provinces.find((p) => p.slug === slug);
+  if (!province) return [];
+  return cities.filter((city) => city.province_id === province.id);
+}
+
+// Find city information by name
+export function getCityByName(name) {
+  return cities.filter((city) => city.name === name);
+}
+
+// Find city information by ID
+export function getCityById(id) {
+  return cities.filter((city) => city.id === id);
+}
+
+// Find city information by slug
+export function getCityBySlug(slug) {
+  return cities.filter((city) => city.slug === slug);
+}
