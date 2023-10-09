df8e222d78b5d2266252921ec1930127baa203a2
navidkhalilian <69416447+navidkhalilian@users.noreply.github.com>
2023-10-09 02:07:39 -0700

    added mssql (#36)
    اضافه شدن فرمت MSSQL

---
+++ sql/mssql/provinces.sql
@@ -0,0 +1,36 @@
+CREATE TABLE provinces(
+   id   INTEGER  NOT NULL 
+  ,name NVARCHAR(19) NOT NULL
+  ,slug NVARCHAR(17) NOT NULL
+);
+INSERT INTO provinces(id,name,slug) VALUES (1,N'آذربایجان شرقی',N'آذربایجان-شرقی');
+INSERT INTO provinces(id,name,slug) VALUES (2,N'آذربایجان غربی',N'آذربایجان-غربی');
+INSERT INTO provinces(id,name,slug) VALUES (3,N'اردبیل',N'اردبیل');
+INSERT INTO provinces(id,name,slug) VALUES (4,N'اصفهان',N'اصفهان');
+INSERT INTO provinces(id,name,slug) VALUES (5,N'البرز',N'البرز');
+INSERT INTO provinces(id,name,slug) VALUES (6,N'ایلام',N'ایلام');
+INSERT INTO provinces(id,name,slug) VALUES (7,N'بوشهر',N'بوشهر');
+INSERT INTO provinces(id,name,slug) VALUES (8,N'تهران',N'تهران');
+INSERT INTO provinces(id,name,slug) VALUES (9,N'چهارمحال و بختیاری',N'چهارمحال-بختیاری');
+INSERT INTO provinces(id,name,slug) VALUES (10,N'خراسان جنوبی',N'خراسان-جنوبی');
+INSERT INTO provinces(id,name,slug) VALUES (11,N'خراسان رضوی',N'خراسان-رضوی');
+INSERT INTO provinces(id,name,slug) VALUES (12,N'خراسان شمالی',N'خراسان-شمالی');
+INSERT INTO provinces(id,name,slug) VALUES (13,N'خوزستان',N'خوزستان');
+INSERT INTO provinces(id,name,slug) VALUES (14,N'زنجان',N'زنجان');
+INSERT INTO provinces(id,name,slug) VALUES (15,N'سمنان',N'سمنان');
+INSERT INTO provinces(id,name,slug) VALUES (16,N'سیستان و بلوچستان',N'سیستان-بلوچستان');
+INSERT INTO provinces(id,name,slug) VALUES (17,N'فارس',N'فارس');
+INSERT INTO provinces(id,name,slug) VALUES (18,N'قزوین',N'قزوین');
+INSERT INTO provinces(id,name,slug) VALUES (19,N'قم',N'قم');
+INSERT INTO provinces(id,name,slug) VALUES (20,N'کردستان',N'کردستان');
+INSERT INTO provinces(id,name,slug) VALUES (21,N'کرمان',N'کرمان');
+INSERT INTO provinces(id,name,slug) VALUES (22,N'کرمانشاه',N'کرمانشاه');
+INSERT INTO provinces(id,name,slug) VALUES (23,N'کهگیلویه و بویراحمد',N'کهگیلویه-بویراحمد');
+INSERT INTO provinces(id,name,slug) VALUES (24,N'گلستان',N'گلستان');
+INSERT INTO provinces(id,name,slug) VALUES (25,N'لرستان',N'لرستان');
+INSERT INTO provinces(id,name,slug) VALUES (26,N'گیلان',N'گیلان');
+INSERT INTO provinces(id,name,slug) VALUES (27,N'مازندران',N'مازندران');
+INSERT INTO provinces(id,name,slug) VALUES (28,N'مرکزی',N'مرکزی');
+INSERT INTO provinces(id,name,slug) VALUES (29,N'هرمزگان',N'هرمزگان');
+INSERT INTO provinces(id,name,slug) VALUES (30,N'همدان',N'همدان');
+INSERT INTO provinces(id,name,slug) VALUES (31,N'یزد',N'یزد');
