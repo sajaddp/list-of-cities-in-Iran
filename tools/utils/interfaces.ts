0187b780488040b63950e6b1d3aeeb05096756df
Sajad Dehshiri <sajaddp71@gmail.com>
2025-01-18 01:15:58 +0330

    Update interfaces.ts

--- tools/utils/interfaces.ts
+++ tools/utils/interfaces.ts
@@ -1,49 +1,49 @@
 export interface ListInterface {
-    type: string,
-    name: string,
-    national_id: number
-    province?: string,
-    city?: string,
-    district?: string,
+  type: string;
+  name: string;
+  national_id: number;
+  province?: string;
+  city?: string;
+  district?: string;
 }
 
 export interface ProvinceInterface {
-    id: number,
-    name: string,
-    slug: string,
-    tel_prefix: string,
+  id: number;
+  name: string;
+  slug: string;
+  tel_prefix: string;
 }
 
 export interface CityInterface {
-    id: number,
-    name: string,
-    slug: string,
-    province_id: number,
+  id: number;
+  name: string;
+  slug: string;
+  province_id: number;
 }
 
 export interface DistrictInterface {
-    id: number,
-    name: string,
-    slug: string,
-    province_id: number,
-    city_id: number,
+  id: number;
+  name: string;
+  slug: string;
+  province_id: number;
+  city_id: number;
 }
 
 export interface RuralInterface {
-    id: number,
-    name: string,
-    slug: string,
-    province_id: number,
-    city_id: number,
-    district_id: number,
+  id: number;
+  name: string;
+  slug: string;
+  province_id: number;
+  city_id: number;
+  district_id: number;
 }
 
 export interface AllInterface {
-    id: number,
-    name: string,
-    slug: string,
-    province_id?: number,
-    city_id?: number,
-    district_id?: number,
-    tel_prefix?: string,
+  id: number;
+  name: string;
+  slug: string;
+  province_id?: number;
+  city_id?: number;
+  district_id?: number;
+  tel_prefix?: string;
 }
