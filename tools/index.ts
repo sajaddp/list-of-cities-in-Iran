e6d8e6fb1cc4e4b782b688659117e7b693298637
Sajad Dehshiri <sajaddp71@gmail.com>
2025-01-18 01:23:36 +0330

    Update index.ts

--- tools/index.ts
+++ tools/index.ts
@@ -2,14 +2,14 @@ import list from "../offical/list.json";
 import * as fs from "fs";
 import * as path from "path";
 import {
-    AllInterface,
-    CityInterface,
-    DistrictInterface,
-    generateSlug,
-    getTelPrefixForProvince,
-    ListInterface,
-    ProvinceInterface,
-    RuralInterface,
+  AllInterface,
+  CityInterface,
+  DistrictInterface,
+  generateSlug,
+  getTelPrefixForProvince,
+  ListInterface,
+  ProvinceInterface,
+  RuralInterface,
 } from "./utils";
 
 const provincesIds = new Map<string, number>();
@@ -23,86 +23,72 @@ const districtsOutput: DistrictInterface[] = [];
 const ruralsOutput: RuralInterface[] = [];
 
 const typeHandlers: { [key: string]: (item: ListInterface) => void } = {
-    "استان": (item) => {
-        provincesIds.set(item.name, item.national_id);
-        provincesOutput.push({
-            id: item.national_id,
-            name: item.name,
-            slug: generateSlug(item.name),
-            tel_prefix: getTelPrefixForProvince(item.name),
-        });
-    },
-    "شهرستان": (item) => {
-        citiesIds.set(item.name, item.national_id);
-        citiesOutput.push({
-            id: item.national_id,
-            name: item.name,
-            slug: generateSlug(item.name),
-            province_id: provincesIds.get(item.province as string) || 0,
-        });
-    },
-    "بخش": (item) => {
-        districtIds.set(item.name, item.national_id);
-        districtsOutput.push({
-            id: item.national_id,
-            name: item.name,
-            slug: generateSlug(item.name),
-            province_id: provincesIds.get(item.province as string) || 0,
-            city_id: citiesIds.get(item.city as string) || 0,
-        });
-    },
-    "دهستان": (item) => {
-        ruralIds.set(item.name, item.national_id);
-        ruralsOutput.push({
-            id: item.national_id,
-            name: item.name,
-            slug: generateSlug(item.name),
-            province_id: provincesIds.get(item.province as string) || 0,
-            city_id: citiesIds.get(item.city as string) || 0,
-            district_id: districtIds.get(item.district as string) || 0,
-        });
-    },
+  استان: (item) => {
+    provincesIds.set(item.name, item.national_id);
+    provincesOutput.push({
+      id: item.national_id,
+      name: item.name,
+      slug: generateSlug(item.name),
+      tel_prefix: getTelPrefixForProvince(item.name),
+    });
+  },
+  شهرستان: (item) => {
+    citiesIds.set(item.name, item.national_id);
+    citiesOutput.push({
+      id: item.national_id,
+      name: item.name,
+      slug: generateSlug(item.name),
+      province_id: provincesIds.get(item.province as string) || 0,
+    });
+  },
+  بخش: (item) => {
+    districtIds.set(item.name, item.national_id);
+    districtsOutput.push({
+      id: item.national_id,
+      name: item.name,
+      slug: generateSlug(item.name),
+      province_id: provincesIds.get(item.province as string) || 0,
+      city_id: citiesIds.get(item.city as string) || 0,
+    });
+  },
+  دهستان: (item) => {
+    ruralIds.set(item.name, item.national_id);
+    ruralsOutput.push({
+      id: item.national_id,
+      name: item.name,
+      slug: generateSlug(item.name),
+      province_id: provincesIds.get(item.province as string) || 0,
+      city_id: citiesIds.get(item.city as string) || 0,
+      district_id: districtIds.get(item.district as string) || 0,
+    });
+  },
 };
 
 (list as ListInterface[]).forEach((item: ListInterface) => {
-    const handler = typeHandlers[item.type];
-    if (handler) {
-        handler(item);
-    }
+  const handler = typeHandlers[item.type];
+  if (handler) {
+    handler(item);
+  }
 });
 
 const allOutput: AllInterface[] = [
-    ...provincesOutput,
-    ...citiesOutput,
-    ...districtsOutput,
-    ...ruralsOutput,
+  ...provincesOutput,
+  ...citiesOutput,
+  ...districtsOutput,
+  ...ruralsOutput,
 ];
 
 fs.mkdirSync(path.join(__dirname, "../json"), { recursive: true });
 
-const provincesOutputPath = path.join(__dirname, "../json/provinces.json");
-fs.writeFileSync(
-    provincesOutputPath,
-    JSON.stringify(provincesOutput, null, 2),
-    "utf-8",
-);
-const citiesOutputPath = path.join(__dirname, "../json/cities.json");
-fs.writeFileSync(
-    citiesOutputPath,
-    JSON.stringify(citiesOutput, null, 2),
-    "utf-8",
-);
-const districtsOutputPath = path.join(__dirname, "../json/districts.json");
-fs.writeFileSync(
-    districtsOutputPath,
-    JSON.stringify(districtsOutput, null, 2),
-    "utf-8",
-);
-const ruralsOutputtPath = path.join(__dirname, "../json/rurals.json");
-fs.writeFileSync(
-    ruralsOutputtPath,
-    JSON.stringify(ruralsOutput, null, 2),
-    "utf-8",
-);
-const outputPath = path.join(__dirname, "../json/all.json");
-fs.writeFileSync(outputPath, JSON.stringify(allOutput, null, 2), "utf-8");
+const outputs = [
+  { name: "provinces", data: provincesOutput },
+  { name: "cities", data: citiesOutput },
+  { name: "districts", data: districtsOutput },
+  { name: "rurals", data: ruralsOutput },
+  { name: "all", data: allOutput },
+];
+
+outputs.forEach(({ name, data }) => {
+  const outputPath = path.join(__dirname, `../json/${name}.json`);
+  fs.writeFileSync(outputPath, JSON.stringify(data, null, 2), "utf-8");
+});
