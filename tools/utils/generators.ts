f38190335cb769c62edae8cafd60e7b5249a25f7
Sajad Dehshiri <sajaddp71@gmail.com>
2025-01-18 01:52:27 +0330

    Update generators.ts

--- tools/utils/generators.ts
+++ tools/utils/generators.ts
@@ -7,6 +7,7 @@ import {
   ProvinceInterface,
   RuralInterface,
 } from ".";
+import { createObjectCsvWriter } from "csv-writer";
 
 export function generateSlug(item: string): string {
   return item
@@ -37,5 +38,30 @@ export function generateJsonFiles(
   outputs.forEach(({ name, data }) => {
     const outputPath = path.join(__dirname, `../../json/${name}.json`);
     fs.writeFileSync(outputPath, JSON.stringify(data, null, 2), "utf-8");
+  });
+}
+export async function generateCsvFiles(
+  allOutput: AllInterface[],
+  provincesOutput: ProvinceInterface[],
+  citiesOutput: CityInterface[],
+  districtsOutput: DistrictInterface[],
+  ruralsOutput: RuralInterface[],
+) {
+  fs.mkdirSync(path.join(__dirname, "../../csv"), { recursive: true });
+
+  const outputs = [
+    { name: "provinces", data: provincesOutput },
+    { name: "cities", data: citiesOutput },
+    { name: "districts", data: districtsOutput },
+    { name: "rurals", data: ruralsOutput },
+    { name: "all", data: allOutput },
+  ];
+
+  outputs.forEach(async ({ name, data }) => {
+    const csvWriter = createObjectCsvWriter({
+      path: path.join(__dirname, `../../csv/${name}.csv`),
+      header: Object.keys(data).map((key) => ({ id: key, title: key })),
+    });
+    await csvWriter.writeRecords(data);
   });
 }
