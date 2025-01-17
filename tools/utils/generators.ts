9f0e98c746572ba186579c9d490a4fb5767dc1f8
Sajad Dehshiri <sajaddp71@gmail.com>
2025-01-18 01:15:26 +0330

    Update generators.ts

--- tools/utils/generators.ts
+++ tools/utils/generators.ts
@@ -1,7 +1,8 @@
 export function generateSlug(item: string): string {
-    return item.replace(/[\u200C\u200B]/g, "")
-        .toLowerCase()
-        .replace(/\s+/g, '-')
-        .replace(/[-]+/g, '-')
-        .replace(/[^\w\-آ-ی\u0600-\u06FF]+/g, '');
+  return item
+    .replace(/[\u200C\u200B]/g, "")
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[-]+/g, "-")
+    .replace(/[^\w\-آ-ی\u0600-\u06FF]+/g, "");
 }
