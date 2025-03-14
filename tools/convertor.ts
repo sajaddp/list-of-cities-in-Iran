0c7b23c3bdea6487fc545eb99aa575bb4bf1345c
Sajad Dehshiri <sajaddp71@gmail.com>
2025-03-15 01:38:05 +0330

    Create convertor.ts

---
+++ tools/convertor.ts
@@ -0,0 +1,19 @@
+import * as xlsx from "xlsx";
+import * as path from "path";
+import * as fs from "fs";
+
+async function convertXlsxToCsv() {
+  const filePath = path.join(__dirname, "../offical/list.xlsx");
+  const workbook = xlsx.readFile(filePath);
+  const sheetName = workbook.SheetNames[0];
+  const worksheet = workbook.Sheets[sheetName];
+  const csv = xlsx.utils.sheet_to_csv(worksheet);
+  const outputFilePath = path.join(__dirname, "../offical/list.csv");
+  fs.writeFileSync(outputFilePath, csv);
+
+  console.log("CSV file created successfully:", outputFilePath);
+}
+
+async () => {
+  await convertXlsxToCsv();
+};
